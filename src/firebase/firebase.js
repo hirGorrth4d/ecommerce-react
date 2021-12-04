5439fb946202b5219eaa335a3c59c83912172522
sonovdragons <cagliolom@gmail.com>
2021-12-04 16:51:53 -0300

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,8 +1,15 @@
 
-import firebase from 'firebase/app';
+import * as firebase from 'firebase/app';
 import 'firebase/firestore';
+import { initializeApp } from "firebase/app";
 
 let firebaseConfig ={
+    apiKey: "AIzaSyAUkkZzw3ZLsPDg4RBp4i-drW-ibaf7dM0",
+    authDomain: "tienda-fisp-ecommerce.firebaseapp.com",
+    projectId: "tienda-fisp-ecommerce",
+    storageBucket: "tienda-fisp-ecommerce.appspot.com",
+    messagingSenderId: "83369511921",
+    appId: "1:83369511921:web:6df022f7acc81896f61250"
 
 }
 
