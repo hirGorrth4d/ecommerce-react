1b4b6acf858383c413f00b3d874cae01878f9114
sonovdragons <cagliolom@gmail.com>
2021-12-29 15:55:21 -0300

    .

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,9 @@
 import { useCartContext } from "../../context/cartContext";
 import {Container, Row, Col} from 'react-bootstrap'
-import db from '../../firebase/firebase';
 import {useState} from 'react'
 import firebase from 'firebase'
 import 'firebase/firestore'
+import getFirestore from '../../firebase/firebase';
 
 
 function Cart(Items){
@@ -19,39 +19,38 @@ function Cart(Items){
 
         orden.date = firebase.firestore.Timestamp.fromDate(new Date());
 
-        orden.buyer = {nombre: 'Martin', email: 'cm@gmail', tel: '12345689'}
+        orden.buyer = {nombre: "asd", email: "asd@gs", tel: "45456"}
         orden.total = precioTotal();
 
         orden.items = cartList.map(Items => {
-            const id= Items.id
-            const nombre= Items.nombre
-            const precio= Items.precio*Items.cantidad
-            return {id: id ,nombre: nombre, precio: precio}
+
+            return {id: Items.id ,nombre: Items.nombre, precio: Items.precio*Items.cantidad}
         })
 
-
-        db.collection('orders').add(orden)
+        const db = getFirestore()
+        db.collection('orden').add(orden)
         .then(resp => setIdOrder(resp.id))
 
-
-        const itemsToUpdate = db.collection('items').where(
-            firebase.firestore.FieldPath.documentId() , 'in', cartList.map(i=> i.id)
-        )
+        // db.collection("ordenes").doc('orden').set(orden)
+        // .then(resp=> setIdOrder(resp.id))
+        // const itemsToUpdate = db.collection('items').where(
+        //     firebase.firestore.FieldPath.documentId() , 'in', cartList.map(i=> i.id)
+        // )
     
-        const batch = db.batch();
-        itemsToUpdate.get()
+        // const batch = db.batch();
+        // itemsToUpdate.get()
 
-        .then( collection=>{
-            collection.docs.forEach(docSnapshot => {
-                batch.update(docSnapshot.ref, {
-                    stock: docSnapshot.data().stock - cartList.find(item => item.id === docSnapshot.id).cantidad
-                })
-            })
+        // .then( collection=>{
+        //     collection.docs.forEach(docSnapshot => {
+        //         batch.update(docSnapshot.ref, {
+        //             stock: docSnapshot.data().stock - cartList.find(item => item.id === docSnapshot.id).cantidad
+        //         })
+        //     })
     
-            batch.commit().then(res =>{
-                console.log('se actualizo')
-            })
-        })
+        //     batch.commit().then(resp =>{
+        //         console.log('se actualizo')
+        //     })
+        // })
     
         
           console.log('verificar cupon')
@@ -105,9 +104,9 @@ function Cart(Items){
             <button onClick={()=> vaciarCarrito()} className="btn btn-outline-primary">Vaciar Carrito</button>
             {`Precio total: ${precioTotal()}`}
             <form onSubmit={generarOrden}>
-                <input type='text' name='name' placeholder='name'/>
-                <input type='text' name='phone'placeholder='tel' />
-                <input type='email' name='email'placeholder='email'/>
+                {/* <input type='text' name='name' placeholder='name' value={formData.name}/>
+                <input type='text' name='phone'placeholder='tel' value={formData.tel} />
+                <input type='email' name='email'placeholder='email' value={formData.email}/> */}
                 <button className="btn btn-outline-primary">Enviar Orden</button>
             </form>
 
